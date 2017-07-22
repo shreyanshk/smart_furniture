acde0b65e8e45a50396ce433c97e03eda7994e85
Shreyansh Khajanchi <shreyansh_k@live.com>
2017-07-22 22:45:30 +0530

    Initial commit

---
+++ run.py
@@ -0,0 +1,16 @@
+import requests
+import RPi.GPIO as gpio
+
+
+def read_ultrasonic_sensor():
+ 	# do stuff
+	pass
+
+
+def client():
+	# do some more stuff
+	pass
+
+
+if __name__ == '__main__':
+	client()
