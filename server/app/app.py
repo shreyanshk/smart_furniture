f94394ec446b9276af8a4b211fcbf95e89b5f52a
Shreyansh Khajanchi <shreyansh_k@live.com>
2017-11-06 23:12:35 +0530

    db: initialize database on start

--- server/app/app.py
+++ server/app/app.py
@@ -3,13 +3,14 @@ from flask_sqlalchemy_session import flask_scoped_session
 
 from .webui import webui
 from .api import api
-from .dbmodels import Session
+from .dbmodels import Session, initialize_db
 
 
 def create_app():
 	app = Flask(__name__)
 	register_extensions(app)
 	register_blueprints(app)
+	initialize_db()
 	return app
 
 
