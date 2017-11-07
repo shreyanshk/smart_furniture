8582b985b447dcca56052a2ec6e07b16d7fbe8ce
Shreyansh Khajanchi <shreyansh_k@live.com>
2017-11-07 20:17:46 +0530

    update models

--- server/app/api.py
+++ server/app/api.py
@@ -1,7 +1,10 @@
+import hashlib
+from datetime import datetime
+
 from flask import Blueprint, request
 from flask_sqlalchemy_session import current_session
 
-from .dbmodels import Event
+from .dbmodels import Event, User
 import json
 
 
@@ -11,8 +14,15 @@ api = Blueprint('api', __name__)
 @api.route('/api/record_event', methods=['POST'])
 def record_event():
 	data = json.loads(request.data)
-	print(data)
-	event = Event(**data)
+	try:
+		event = data['event']
+		eventtype = data['eventtype']
+		deviceid = data['deviceid']
+		timestamp = data['timestamp']
+	except KeyError as e:
+		return '', 404
+	timestamp = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
+	event = Event(event=event, eventtype=eventtype, timestamp=timestamp, deviceid=deviceid)
 	current_session.add(event)
 	current_session.commit()
 	return '', 200
@@ -20,15 +30,28 @@ def record_event():
 
 @api.route('/api/user_events', methods=['GET'])
 def return_user_events():
-	print("starting execution")
-	clientid = request.values.get('clientid')
-	print("got clientid")
+	try:
+		deviceid = request.values.get('deviceid')
+	except KeyError:
+		return "invalid request", 404
 	data = current_session.query(Event).filter(
-		Event.clientid == clientid,
+		Event.deviceid == deviceid,
 	).all()
-	print("got data")
 	data = [record.as_dict() for record in data]
-	print("making dict")
 	data = json.dumps(data)
-	print("returning data")
 	return data, 200
+
+
+@api.route('/api/user_mgmt/add_user', methods=['POST'])
+def add_user_to_system():
+	try:
+		username = request.values['username']
+		password = request.values['password']
+		deviceid = request.values['deviceid']
+	except KeyError:
+		return "invalid request", 404
+	pwhash = hashlib.sha512(password).hexdigest()
+	userrecord = User(username=username, pwhash=pwhash, clientid=deviceid)
+	current_session.add(userrecord)
+	current_session.commit()
+	return '', 200
--- server/app/dbmodels.py
+++ server/app/dbmodels.py
@@ -1,41 +1,50 @@
-from sqlalchemy import Column, Integer, String, TIMESTAMP
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-engine = create_engine('sqlite://')
+engine = create_engine('sqlite:////tmp/iotdb.sqlite3')
 Base = declarative_base()
 Session = scoped_session(sessionmaker(bind=engine))
 
 
 class Event(Base):
-	__tablename__ = "iot_events"
+	__tablename__ = "events"
 	id = Column(Integer, primary_key=True)
-	type = Column(String, nullable=False)
+	eventtype = Column(String, nullable=False)
 	event = Column(String, nullable=False)
-	clientid = Column(String, nullable=False)
-	timestamp = Column(TIMESTAMP, nullable=False)
+	deviceid = Column(String, nullable=False)
+	timestamp = Column(DateTime, nullable=False)
 
 	def __init__(self, **kwargs):
-		self.type = kwargs['type']
+		self.eventtype = kwargs['eventtype']
 		self.event = kwargs['event']
-		self.clientid = kwargs['clientid']
+		self.deviceid = kwargs['deviceid']
 		self.timestamp = kwargs['timestamp']
 
 	def as_dict(self):
 		return {
-			'type': self.type,
+			'eventtype': self.eventtype,
 			'event': self.event,
-			'clientid': self.clientid,
+			'deviceid': self.deviceid,
 			'timestamp': str(self.timestamp),
 		}
+
+
+class User(Base):
+	__tablename__ = 'users'
+	id = Column(Integer, primary_key=True)
+	username = Column(String, nullable=False)
+	pwhash = Column(String, nullable=False)
+	deviceid = Column(String, nullable=False)
+
+	def __init__(self, **kwargs):
+		self.username = kwargs['username']
+		self.pwhash = kwargs['pwhash']
+		self.deviceid = kwargs['deviceid']
 
 
 def initialize_db():
 	Base.metadata.drop_all(engine)
 	Base.metadata.create_all(engine)
-	print('reinitialized db tables')
-
-
-if __name__ == '__main__':
-	initialize_db()
+	print('initialized db tables')
